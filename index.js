f004f226abeda45fb96a9349a66379567803d5ea
shahzad akram <alyakram786@gmail.com>
2024-01-25 16:37:30 +0500

    almost completed

--- index.js
+++ index.js
@@ -27,57 +27,59 @@ let arr = [{
         img: "./person-5.jpeg",
         name: 'Lola Gardner ',
         year: "29 years"
+    }, {
+        img: "./person-5.jpeg",
+        name: 'Lola Gardner ',
+        year: "29 years"
     }
 
+
+
 ]
-doc.innerHTML += `
-<h3 id="h1">5 Birthdays Today</h3>`;
-let h3 = document.getElementById("h3")
+doc.innerHTML = `<h3 id="h1">${arr.length} Birthdays Today</h3>`;
+
+// let h3 = document.getElementById("h3")
 
 for (let i = 0; i < arr.length; i++) {
-    // console.log(arr)
+    doc2.style.display = "none"
+        // console.log(arr
     doc.innerHTML += `
-
-<
-div id = "divjs" >
-    <
-    img src = $ { arr[i].img }
-id = "img" >
-    <
-    div id = "divjs2" >
-    <
-    h5 class = "text" > $ { arr[i].name } < /h5> <
-    p class = "text" > $ { arr[i].year } < /p> <
-    /div> <
-    /div>
-`
+    
+    <div id= "divjs"> 
+    <img src=${arr[i].img} id="img">
+    <div id="divjs2">
+    <h5 class= "text"> ${arr[i].name } </h5>
+    <p class= "text">${arr[i].year}</p>
+    </div>
+    </div>
+    `
 }
 
-doc.innerHTML += ` <
-h3 id = "h1" > 5 Birthdays Today < /h3>
-
-<
-button id = "clrbtn" > Clear All < /button>
-`;
+doc.innerHTML += `<button id="clrbtn">Clear All</button>`;
 // 
 let btnclear = document.getElementById("clrbtn");
 
 
-
 btnclear.addEventListener('click', () => {
+    // arr.clear;
+    // doc2.style.display =
+    // doc.style.display = "none";
+    // arr[i].splice(0, null);
+    arr.length = 0;
+    doc.innerHTML = `<h3 id="h1">${arr.length} Birthdays Today</h3>`;
+    doc.style.height = "20vh";
 
-    doc.style.display = "none";
-    doc2.style.display = block
-    doc2.innerHTML += ` <
-div id = "container2" >
-    <
-    h3 id = "h1" > 5 Birthdays Today < /h3>
 
-<
-button id = "clrbtn2" > Clear All < /button> <
-    /div>
 
-`
+    // doc2.innerHTML += `
+    // <div class = "containerjs3">
+    // <h3 id="h1">${arr.length = 0} Birthdays Today</h3>
+
+    // </div>
+    // `
 
     // doc.style.display = ""
 })
+
+
+doc2.innerHTML = `<h3 id="h1">0 Birthdays Today</h3>`;
