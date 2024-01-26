aa053df04fc65c09d9a3e90d2028ad5e2d8f708a
shahzad akram <alyakram786@gmail.com>
2024-01-26 15:18:12 +0500

    adding back button

--- index.js
+++ index.js
@@ -31,6 +31,21 @@ let arr = [{
         img: "./person-5.jpeg",
         name: 'Lola Gardner ',
         year: "29 years"
+    },
+    {
+        img: "./person-5.jpeg",
+        name: 'Lola Gardner ',
+        year: "29 years"
+    },
+    {
+        img: "./person-5.jpeg",
+        name: 'Lola Gardner ',
+        year: "29 years"
+    },
+    {
+        img: "./person-5.jpeg",
+        name: 'Lola Gardner ',
+        year: "29 years"
     }
 
 
@@ -38,11 +53,10 @@ let arr = [{
 ]
 doc.innerHTML = `<h3 id="h1">${arr.length} Birthdays Today</h3>`;
 
-// let h3 = document.getElementById("h3")
+
 
 for (let i = 0; i < arr.length; i++) {
-    // doc2.style.display = "none"
-    // console.log(arr
+
     doc.innerHTML += `
     
     <div id= "divjs"> 
@@ -56,31 +70,81 @@ for (let i = 0; i < arr.length; i++) {
 }
 
 doc.innerHTML += `<button id="clrbtn">Clear All</button>`;
-// 
+
 let btnclear = document.getElementById("clrbtn");
 
 
-btnclear.addEventListener('click', () => {
-    // arr.clear;
-    // doc2.style.display =
-    // doc.style.display = "none";
-    // arr[i].splice(0, null);
-
-    // arr.length = 0;
-    // or
+btnclear.addEventListener('click', () => {;
     arr = "";
-    doc.innerHTML = `<h3 id="h1">${arr.length} Birthdays Today</h3>`;
+    doc.innerHTML = `<h3 id="h1">${arr.length} Birthdays Today</h3> <i id="icon" class = "fa-solid fa-arrows-rotate"> </i>`;
     doc.style.height = "20vh";
+    doc.innerHTML += `<button id="clrbtn">Back</button>`;
+    // doc.style.margintop = "20px";
 
 
 
-    // doc2.innerHTML += `
-    // <div class = "containerjs3">
-    // <h3 id="h1">${arr.length = 0} Birthdays Today</h3>
 
-    // </div>
-    // `
+})
 
-    // doc.style.display = ""
+
+
+
+let arr2 = [{
+        img: "./person-1.jpeg",
+        name: "Bertie Yates",
+        year: "29 years"
+    },
+    {
+        img: "./person-2.jpeg",
+        name: 'Herster Hogan',
+        year: "32 years"
+    },
+    {
+        img: "./person-3.jpeg",
+        name: 'Larry Little',
+        year: "36 years"
+    },
+    {
+        img: "./person-4.jpeg",
+        name: 'Sean Walsh',
+        year: "34 years"
+    },
+    {
+        img: "./person-5.jpeg",
+        name: 'Lola Gardner ',
+        year: "29 years"
+    }, {
+        img: "./person-5.jpeg",
+        name: 'Lola Gardner ',
+        year: "29 years"
+    },
+    {
+        img: "./person-5.jpeg",
+        name: 'Lola Gardner ',
+        year: "29 years"
+    },
+    {
+        img: "./person-5.jpeg",
+        name: 'Lola Gardner ',
+        year: "29 years"
+    },
+    {
+        img: "./person-5.jpeg",
+        name: 'Lola Gardner ',
+        year: "29 years"
+    }
+]
+let icon = document.getElementById("icon");
+icon.addEventListener("click", () => {
+    doc.innerHTML += `
+    
+    <div id= "divjs"> 
+    <img src=${arr2[i].img} id="img">
+    <div id="divjs2">
+    <h5 class= "text"> ${arr2[i].name } </h5>
+    <p class= "text">${arr2[i].year}</p>
+    </div>
+    </div>
+    `
 
 })
